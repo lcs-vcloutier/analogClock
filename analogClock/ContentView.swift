34ddcc30ad8b11a4125d10e0549166700812c2dc
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-27 13:39:42 -0400

    Initial Commit

---
+++ analogClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  analogClock
+//
+//  Created by Cloutier, Vincent on 2020-10-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ analogClock/analogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  analogClockApp.swift
+//  analogClock
+//
+//  Created by Cloutier, Vincent on 2020-10-27.
+//
+
+import SwiftUI
+
+@main
+struct analogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
